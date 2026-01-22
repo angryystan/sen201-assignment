ba101f1e160e478d139858493f898a99d9c186e4
angryystan <oladiranstanleywale@gmail.com>
2026-01-22 23:12:19 +0100

    Add metadata comments to hello.py
    Added metadata comments to hello.py.

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
 print("Hello, World")
+# SEN 201 - Hello World
+
+**Name:** Oladiran Stanley Wale  
+**Matric Number:** 24/15980  
+**Description:** Simple Python program that prints "Hello, World".
